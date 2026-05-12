0569c8409c87716ef96759fb53d896cabd1b0cd9
“josephsinamo” <josephsinamo2@gmail.com>
2026-05-12 17:31:36 +0700

    up untuk frame tipis tipis

--- src/view/Frame.java
+++ src/view/Frame.java
@@ -1,6 +1,49 @@
 
 package view;
 
-public class Frame {
-    
+import java.awt.*;
+import javax.swing.*;
+
+public class Frame extends JFrame{
+
+    private CardLayout cardLayout;
+    private JPanel mainPanel
+    ;
+    public Frame() {
+
+        setTitle("Game Restoran");;
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        cardLayout = new CardLayout();
+        mainPanel = new JPanel(cardLayout);
+
+
+        tampilkanLayarPenuh();
+    }
+
+
+    public void tampilkanLayarPenuh() {
+        
+        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        this.setVisible(true);
+
+        /* untuk fullScreen
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        GraphicsDevice gd = ge.getDefaultScreenDevice();
+
+        if (gd.isFullScreenSupported()) {
+            gd.setFullScreenWindow(this);
+        } else {
+            System.err.println("Full screen tidak didukung, menggunakan mode Maksimal.");
+            this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+            this.setVisible(true);
+        }
+         */
+    }
+}
+
+class mainSementara{
+    public static void main(String[] args) {
+        Frame f = new Frame();
+    }
 }
